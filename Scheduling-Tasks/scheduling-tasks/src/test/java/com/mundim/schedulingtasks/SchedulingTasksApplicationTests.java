f630cdfb8fddb1d2e58694b7715956d62caf7259
BrunoMundim <brunomundimfranco@gmail.com>
2022-11-09 22:04:33 -0300

    Scheduling Tasks

---
+++ Scheduling-Tasks/scheduling-tasks/src/test/java/com/mundim/schedulingtasks/SchedulingTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mundim.schedulingtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
